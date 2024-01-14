988bdefaa5b91234368567f608ee356c6898f149
kifah <kifah.andary@gmail.com>
2024-01-14 14:24:49 +0200

    added multiple nft for the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   Box,
   Container,
@@ -13,7 +14,6 @@ import {
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function Home() {
   const [nfts, setNfts] = useState([]);
@@ -73,8 +73,8 @@ export default function Home() {
       <Container sx={{ py: 8 }} maxWidth="md">
         {/* End hero unit */}
         <Grid container spacing={4}>
-          {cards.map((card) => (
-            <Grid item key={card} xs={12} sm={6} md={4}>
+          {nfts.map((card: any) => (
+            <Grid item key={card?.nft?.id} xs={12} sm={6} md={4}>
               <Card
                 sx={{
                   height: "100%",
@@ -88,19 +88,19 @@ export default function Home() {
                     // 16:9
                     pt: "56.25%",
                   }}
-                  image="https://source.unsplash.com/random?wallpapers"
+                  image={card?.metadata?.image}
                 />
                 <CardContent sx={{ flexGrow: 1 }}>
                   <Typography gutterBottom variant="h5" component="h2">
-                    Heading
+                    {card?.metadata?.name}
                   </Typography>
-                  <Typography>
-                    This is a media card. You can use this section to describe
-                    the content.
-                  </Typography>
+                  <Typography>{card?.metadata?.description}</Typography>
                 </CardContent>
                 <CardActions>
-                  <Button size="small" onClick={() => nav(`/nft/${card}`)}>
+                  <Button
+                    size="small"
+                    onClick={() => nav(`/nft/${card?.nft?.id}`)}
+                  >
                     View
                   </Button>
                 </CardActions>
